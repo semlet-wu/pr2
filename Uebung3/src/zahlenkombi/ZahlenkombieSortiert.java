483ba8d05087a17198eaf0b05bb0c29001366773
Jan Schreiber <jan.schreib@prae.me>
2013-04-18 10:16:11 +0200

    omg

--- Uebung3/src/zahlenkombi/ZahlenkombieSortiert.java
+++ Uebung3/src/zahlenkombi/ZahlenkombieSortiert.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public class ZahlenkombieSortiert extends Zahlenkombi{
 	
 	private int[] zahlen;
-	private static ArrayList<Double> timings = new ArrayList<Double>();
+	private static ArrayList<Long> timings = new ArrayList<Long>();
 	public ZahlenkombieSortiert(int max) {
 		super(max);
 		
@@ -21,28 +21,28 @@ public class ZahlenkombieSortiert extends Zahlenkombi{
 	
 	@Override public boolean istEnthalten(int zahl){
 		
-		double start= System.currentTimeMillis();  
+		long start= System.currentTimeMillis();  
     	 
     	if(timings.isEmpty()){
     		Arrays.sort(zahlen);
-    		double ende1= System.currentTimeMillis();
+    		long ende1= System.currentTimeMillis();
     		timings.add(ende1-start); 
     	}
     	 
     	else{
     		
     		Arrays.binarySearch(zahlen, zahl);
-    		double ende2= System.currentTimeMillis();  
+    		long ende2= System.currentTimeMillis();  
     		timings.add(ende2-start);  
         	return true;
         }
     	return false;
 	}
     public void zeitmessung(){
-    	double time= 0.0;
-    	for(Double d : timings){
+    	long time = 0;
+    	for(Long d : timings){
     		time += d;
     	} 
-    	System.out.println("Zeit: " + time/timings.size() + "ms");
+    	System.out.println("Zeit: " + (double)time/timings.size() + "ms");
     }
 }
